46585e6db9bf8b3f842bde8e10342fa420cd499e
Tatyana Kutalo <tatyana@kutalo.com>
2020-07-24 20:23:00 +0300

    HttpClientModule and HttpClientInMemoryWebApiModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientModule } from '@angular/common/http';
+import { InMemoryDataService } from './in-memory-data.service';
+import { NgModule } from '@angular/core';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-  ],
-  imports: [
-    BrowserModule,
-
-    FormsModule,
-
-  ],
-  providers: [
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HeroesComponent],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+            dataEncapsulation: false,
+            passThruUnknownUrl: true,
+            put204: false,
+        }),
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
